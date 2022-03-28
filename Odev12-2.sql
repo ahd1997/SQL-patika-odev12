65d375dc3025351f07afde06487163f46ba0a8b7
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-28 13:05:45 +0300

    Update Odev12-2.sql

--- Odev12-2.sql
+++ Odev12-2.sql
@@ -1,1 +1,3 @@
-dvdrental örnek veri tabanı üzerinden film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+--dvdrental örnek veri tabanı üzerinden film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+SELECT COUNT(title) FROM film
+WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
