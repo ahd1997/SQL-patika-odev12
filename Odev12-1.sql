9aaa0110ae4f3409ecedd367758b2094e1af8ed0
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-28 13:03:45 +0300

    Update Odev12-1.sql

--- Odev12-1.sql
+++ Odev12-1.sql
@@ -1,1 +1,3 @@
 --dvdrental örnek veri tabanı üzerinden film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+SELECT COUNT(title) FROM film
+WHERE length > (SELECT AVG(length) FROM film);
