eadd03ed69b97c8d41f9f299a5d283f51546fd01
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-28 13:25:35 +0300

    Update Odev12-4.sql

--- Odev12-4.sql
+++ Odev12-4.sql
@@ -1,1 +1,4 @@
 --dvdrental örnek veri tabanı üzerinden payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+SELECT customer.first_name,customer.last_name,payment.customer_id FROM payment
+FULL JOIN customer ON payment.customer_id = customer.customer_id
+WHERE payment.amount = (SELECT MAX(amount)FROM payment);
