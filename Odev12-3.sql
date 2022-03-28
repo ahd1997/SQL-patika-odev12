334cad1e270ce1c9aaa4dc13a005fc0f5f438c43
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-28 13:13:09 +0300

    Update Odev12-3.sql

--- Odev12-3.sql
+++ Odev12-3.sql
@@ -1,1 +1,4 @@
---dvdrental örnek veri tabanı üzerinden film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+--dvdrental örnek veri tabanı üzerinden film tablosunda en düşük rental_rate ve en düşük replacement_cost değerlerine sahip filmleri sıralayınız.
+SELECT title FROM film
+WHERE rental_rate = (SELECT MIN(rental_rate) FROM film)
+AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
